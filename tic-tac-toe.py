7b7a280602bcede7a5f8499c960215993e51650a
sc23nen <sc23nen@leeds.ac.uk>
2024-08-04 15:37:04 +0100

    function creation

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -9,8 +9,40 @@ def display_board():
     print(f"{board[6]} | {board[7]} | {board[8]}")
 
 def play_game():
+    game_still_going = False
     # Display the initial board
     display_board()
+
+    while game_still_going:
+        handle_turn(current_player)
+
+        check_end_of_game()
+
+        flip_player()
+
+
+def handle_turn():
+    position = input("Make your move. \nChoose a position from 1-9: ")
+    position = int(position) - 1
+
+    board[position] = "X"
+
+def check_end_of_game():
+    check_win()
+    check_tie()
+
+def check_win():
+    # check rows
+    # check columns
+    # check diagonals
+    return
+
+def check_tie():
+
+    return
+
+def flip_player():
+    return
 
 # board
 # Display board
